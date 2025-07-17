98edd0ef32cf36078bc644ea49c52cb3872e4830
Mainak <mainak1428@gmail.com>
2025-07-18 01:27:58 +0530

    vercel deployment init

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // //@ts-ignore
-    // allowedDevOrigins: ['192.168.0.101:3000']
+  /* config options here */
+  eslint:{
+    ignoreDuringBuilds: true,
   }
 };
 
