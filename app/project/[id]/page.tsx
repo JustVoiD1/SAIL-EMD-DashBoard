f44d2a99177bf1c1366a3d6c5a4cf9a1146bc6a9
Mainak <mainak1428@gmail.com>
2025-08-29 23:44:52 +0530

    added delete feature

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -161,7 +161,7 @@ const page = () => {
 
         }
       } catch (err) {
-        setError("Failed to Fetch Project: ", err)
+        setError("Failed to Fetch Project")
       } finally {
         setLoading(false)
       }
